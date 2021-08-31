8a5de5814d5d075e6a8465835d75c182fbcfd0ea
vladtenlive <vlad010694@gmail.com>
2021-08-31 14:30:46 +0500

    38_refactor

--- youtube/38_matrix_zeroes.py
+++ youtube/38_matrix_zeroes.py
@@ -3,30 +3,35 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        first_column = False
+        is_row = False
+        is_column = False
+
         rows = len(matrix)
         columns = len(matrix[0])
-        
+
         for i in range(rows):
-          
           if matrix[i][0] == 0:
-            first_column = True
-          
+            is_column = True
+
+        for j in range(columns):
+          if matrix[0][j] == 0:
+            is_row = True
+
+        for i in range(1, rows):
           for j in range(1, columns):
             if matrix[i][j] == 0:
               matrix[i][0] = 0
               matrix[0][j] = 0
-              
-              
+
         for i in range(1, rows):
           for j in range(1, columns):
             if matrix[i][0] == 0 or matrix[0][j] == 0:
               matrix[i][j] = 0
-           
-        if matrix[0][0] == 0:
-          for j in range(columns):
-            matrix[0][j] = 0
-              
-        if first_column:
+
+        if is_column:
           for i in range(rows):
             matrix[i][0] = 0
+
+        if is_row:
+          for j in range(columns):
+            matrix[0][j] = 0
